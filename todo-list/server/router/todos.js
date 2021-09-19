37cb8e1df609ddd59be9e5ded9f0e4f4291a77d4
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2021-09-19 17:42:22 +0900

    ✨ CRD기능 구현

---
+++ todo-list/server/router/todos.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import 'express-async-errors';
+import { body } from 'express-validator';
+import * as todoController from '../controller/todos.js';
+import { validationResult } from 'express-validator';
+
+const router = express.Router();
+
+const validate = (req, res, next) => {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    return next();
+  }
+  return res.status(400).json({ message: errors.array()[0].msg });
+};
+
+const validateTodo = [
+  body('contents')
+    .trim()
+    .isLength({ min: 2 })
+    .withMessage('2글자 이상이어야합니다!'),
+  validate,
+];
+
+router.get('/', todoController.getTodos);
+
+router.post('/', validateTodo, todoController.createTodo);
+router.delete('/:id', todoController.deleteTodo);
+
+export default router;
