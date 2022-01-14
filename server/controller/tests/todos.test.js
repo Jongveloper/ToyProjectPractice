ecad8d1f9ac682203eb5256a107fb3561c535d6c
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2022-01-15 00:28:13 +0900

    ✅delete Todo 테스트 통과

--- server/controller/tests/todos.test.js
+++ server/controller/tests/todos.test.js
@@ -51,4 +51,40 @@ describe('TodoController', () => {
       expect(todosRepository.create).toHaveBeenCalledWith(newTodo);
     });
   });
+
+  describe('투두를 삭제합니다.', () => {
+    let todoId, request, response;
+    beforeEach(() => {
+      todoId = faker.random.alphaNumeric(16);
+      request = httpMocks.createRequest({
+        params: { id: todoId },
+      });
+      response = httpMocks.createResponse();
+    });
+
+    it('투두리스트를 삭제합니다.', async () => {
+      todosRepository.getById = () => ({
+        todoId,
+      });
+      todosRepository.remove = jest.fn();
+
+      await todoController.deleteTodo(request, response);
+
+      expect(response.statusCode).toBe(204);
+      expect(todosRepository.remove).toHaveBeenCalledWith(todoId);
+    });
+
+    it('해당하는 아이디가 없으면 404를 반환합니다.', async () => {
+      todosRepository.getById = () => undefined;
+      todosRepository.remove = jest.fn();
+
+      await todoController.deleteTodo(request, response);
+
+      expect(response.statusCode).toBe(404);
+      expect(response._getJSONData()).toMatchObject({
+        message: `해당 아이디가 없습니다 : ${todoId}`,
+      });
+      expect(todosRepository.remove).not.toHaveBeenCalledWith();
+    });
+  });
 });
