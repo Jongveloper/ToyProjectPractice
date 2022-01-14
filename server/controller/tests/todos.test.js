c953a2e46d70bf655c956fb06d556b45f0b3cb39
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2022-01-15 00:18:21 +0900

    ✅create Todo 테스트 통과

--- server/controller/tests/todos.test.js
+++ server/controller/tests/todos.test.js
@@ -27,4 +27,28 @@ describe('TodoController', () => {
       expect(response._getJSONData()).toEqual(allTodos);
     });
   });
+
+  describe('createTodos', () => {
+    let newTodo, request, response;
+    beforeEach(() => {
+      newTodo = faker.random.words(3);
+      request = httpMocks.createRequest({
+        body: { contents: newTodo },
+      });
+      response = httpMocks.createResponse();
+    });
+
+    it('투두리스트가 생성됩니다.', async () => {
+      todosRepository.create = jest.fn((contents) => ({
+        contents,
+      }));
+      await todoController.createTodo(request, response);
+
+      expect(response.statusCode).toBe(201);
+      expect(response._getJSONData()).toMatchObject({
+        contents: newTodo,
+      });
+      expect(todosRepository.create).toHaveBeenCalledWith(newTodo);
+    });
+  });
 });
