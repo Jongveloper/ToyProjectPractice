b557e1ecc0a24ece9d906eab39da48b6e3e49850
Woo Dy <kjop7419@naver.com>
2022-05-24 20:58:21 +0900

    :technologist: Route 구현

--- JSPA/server.js
+++ JSPA/server.js
@@ -3,8 +3,10 @@ const path = require('path');
 
 const app = express();
 
+app.use('/static', express.static(path.resolve(__dirname, 'client', 'static')));
+
 app.get('/*', (req, res) => {
-  res.sendFile(path.resolve('client', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'client', 'index.html'));
 });
 
 app.listen(3000, () => {
