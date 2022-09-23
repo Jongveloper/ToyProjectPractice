b64b389f2f697ce9cf438360e3687bd7d6970512
Woo Dy <kjop7419@naver.com>
2022-09-23 19:57:36 +0900

    드래그 앤 드롭을 구현하라

--- practice/src/App.tsx
+++ practice/src/App.tsx
@@ -21,6 +21,8 @@ const App = () => {
     tasks: [],
   });
 
+  const [grab, setGrab] = useState<any>();
+
   const { newId, taskTitle, tasks } = state;
 
   const handleChangeTitle = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -46,6 +48,43 @@ const App = () => {
     });
   };
 
+  const _onDragOver = (e: React.DragEvent) => {
+    e.preventDefault();
+  };
+
+  const _onDragStart = (e: React.DragEvent<Element>) => {
+    setGrab(e.target);
+    (e.target as Element).classList.add('grabbing');
+    e.dataTransfer.effectAllowed = 'move';
+    e.dataTransfer.setData('text/html', '0');
+  };
+
+  const _onDragEnd = (e: React.DragEvent<Element>) => {
+    (e.target as Element).classList.remove('grabbing');
+
+    e.dataTransfer.dropEffect = 'move';
+  };
+
+  const _onDrop = (e: React.DragEvent<Element>) => {
+    const grabPosition = Number(grab.dataset.position);
+    const targetPosition = Number(
+      (e.target as HTMLDivElement).dataset.position
+    );
+
+    const _list = tasks;
+
+    _list[grabPosition] = _list.splice(
+      targetPosition,
+      1,
+      _list[grabPosition]
+    )[0];
+
+    setState({
+      ...state,
+      tasks: _list,
+    });
+  };
+
   return (
     <>
       <Form
@@ -53,7 +92,14 @@ const App = () => {
         handleChange={handleChangeTitle}
         handleClick={handleClickAddTask}
       />
-      <List tasks={tasks} handleClick={handleClickDeleteTask} />
+      <List
+        onDragOver={_onDragOver}
+        onDragStart={_onDragStart}
+        onDragEnd={_onDragEnd}
+        onDrop={_onDrop}
+        tasks={tasks}
+        handleClick={handleClickDeleteTask}
+      />
     </>
   );
 };
--- practice/src/components/List/index.tsx
+++ practice/src/components/List/index.tsx
@@ -5,15 +5,39 @@ import { Task } from 'App';
 interface ListType {
   handleClick: (number: number) => void;
   tasks: Task[];
+  onDragOver?: (e: React.DragEvent) => void;
+  onDragEnd?: (e: React.DragEvent<Element>) => void;
+  onDragStart?: (e: React.DragEvent<Element>) => void;
+  onDrop?: (e: any) => void;
 }
 
-const List: FC<ListType> = ({ tasks, handleClick }) => {
+const List: FC<ListType> = ({
+  tasks,
+  handleClick,
+  onDragOver,
+  onDragEnd,
+  onDragStart,
+  onDrop,
+}) => {
+  const style = {
+    display: 'flex',
+  };
+
   return (
     <ul>
-      {tasks.map(({ title, id }) => {
+      {tasks.map(({ title, id }, idx) => {
         return (
-          <div key={id}>
-            <li>{title}</li>
+          <div
+            draggable
+            style={style}
+            key={id}
+            data-position={idx}
+            onDragOver={(e: any) => onDragOver!(e)}
+            onDragEnd={(e: any) => onDragEnd!(e)}
+            onDragStart={(e: any) => onDragStart!(e)}
+            onDrop={(e: any) => onDrop!(e)}
+          >
+            <li data-position={idx}>{title}</li>
             <button onClick={() => handleClick(id)}>삭제</button>
           </div>
         );
